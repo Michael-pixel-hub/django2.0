0c3336d34cd2a54b1d95c6b39f917b76553a752b
Michael <kurashevmichael@gmail.com>
2022-09-29 19:02:50 +0300

    lesson_3

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from mainapp.views import MainPageView, CoursesPageView, ContactsPageView, DocSitePageView, LoginPageView, NewsPageView
+from mainapp.views import MainPageView, CoursesPageView, ContactsPageView, DocSitePageView, LoginPageView, news, newspage
+
+app_name = 'mainapp'
 
 urlpatterns = [
-    path('', MainPageView.as_view()),
-    path('courses/', CoursesPageView.as_view()),
-    path('contacts/', ContactsPageView.as_view()),
-    path('docsite/', DocSitePageView.as_view()),
-    path('login/', LoginPageView.as_view()),
-    path('news/', NewsPageView.as_view())
+    path('', MainPageView.as_view(), name='index'),
+    path('courses/', CoursesPageView.as_view(), name='course'),
+    path('contacts/', ContactsPageView.as_view(), name='contacts'),
+    path('docsite/', DocSitePageView.as_view(), name='docsite'),
+    path('login/', LoginPageView.as_view(), name='login'),
+    path('news/', news, name='news'),
+    path('news/<int:page>', newspage, name='newspage')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from datetime import datetime
+from django.core.paginator import Paginator
 
 
 class MainPageView(TemplateView):
@@ -10,8 +13,49 @@ class ContactsPageView(TemplateView):
     template_name = "mainapp/contacts.html"
 
 
-class NewsPageView(TemplateView):
-    template_name = "mainapp/news.html"
+# class NewsPageView(TemplateView):
+#     template_name = "mainapp/news.html"
+#     extra_context = {
+#         'products': [{"name": 'Новость 1', "description": 'Описание 1'},
+#                      {"name": 'Новость 2', "description": 'Описание 2'},
+#                      {"name": 'Новость 3', "description": 'Описание 3'},
+#                      {"name": 'Новость 4', "description": 'Описание 4'},
+#                      {"name": 'Новость 5', "description": 'Описание 5'},
+#                      ],
+#
+#         "date": datetime.now(),
+#     }
+
+
+def news(request):
+    context = {
+        'products': [{"name": 'Новость 1', "description": 'Описание 1'},
+                     {"name": 'Новость 2', "description": 'Описание 2'},
+                     {"name": 'Новость 3', "description": 'Описание 3'},
+                     {"name": 'Новость 4', "description": 'Описание 4'},
+                     {"name": 'Новость 5', "description": 'Описание 5'},
+                     ],
+        "date": datetime.now(),
+    }
+    return render(request, "mainapp/news.html", context)
+
+
+def newspage(request, page=None):
+    context_news = [{"name": 'Новость 1', "description": 'Описание 1'},
+                     {"name": 'Новость 2', "description": 'Описание 2'},
+                     {"name": 'Новость 3', "description": 'Описание 3'},
+                     {"name": 'Новость 4', "description": 'Описание 4'},
+                     {"name": 'Новость 5', "description": 'Описание 5'},
+                     ]
+    paginator = Paginator(context_news, 2)
+    news_paginator = paginator.page(page)
+
+    context = {
+        "products": news_paginator,
+        "date": datetime.now()
+    }
+
+    return render(request, "mainapp/news.html", context)
 
 
 class LoginPageView(TemplateView):
