01f56953a8c92ef9c326ac16adc2c3b560258962
Michael <kurashevmichael@gmail.com>
2022-10-20 17:21:42 +0300

    lesson_9_part_1

--- geekb/celery.py
+++ geekb/celery.py
@@ -1,7 +1,10 @@
 ﻿import os
+from django.conf import settings
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geekb.settings")
+if settings.DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 celery_app = Celery("braniac")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
